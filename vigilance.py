7dc1932ecab89df4729ee6bfb1652823ef712bd5
Mahidhar1515 <121248456+Mahidhar1515@users.noreply.github.com>
2024-11-08 19:42:48 +0530

    Add files via upload

---
+++ vigilance.py
@@ -0,0 +1,89 @@
+import cv2
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+import imutils
+import dlib
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+mixer.init()
+mixer.music.load("music.wav")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+thresh = 0.25
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+cap = cv2.VideoCapture(0)  # 0 for default camera, change if you want to use a different camera
+
+# Email configuration
+email_sender = 'mahidharmygapula@gmail.com'
+email_password = 'rvwp krxr nsdf ffbv'
+email_receivers = ['21r11a05n9@gcet.edu.in','21r11a05m8@gcet.edu.in','21r11a05l9@gcet.edu.in']
+
+def send_email(subject, message):
+    msg = MIMEMultipart()
+    msg['From'] = email_sender
+    msg['To'] = ", ".join(email_receivers)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_sender, email_password)
+    text = msg.as_string()
+    server.sendmail(email_sender, email_receivers, text)
+    server.quit()
+
+flag = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        if ear < thresh:
+            flag += 1
+            if flag >= frame_check:
+                cv2.putText(frame, "****************ALERT!****************", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "****************ALERT!****************", (10, 325),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                mixer.music.play()
+                send_email("Drowsy Driver Alert", "ALERT! Driver is drowsy.")
+        else:
+            flag = 0
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
